f73ac8b4abc7c979d96b9bcbe753e4b9ecb1c8ea
Edgleidson <wake-up88@hotmail.com>
2022-07-14 21:44:13 -0300

    Alterando o title e o footer do projeto

---
+++ views/home/index.php
@@ -0,0 +1,27 @@
+
+           <?php $this->load->view('layout/navbar');?>
+
+            <div class="page-wrap">
+
+           
+            <?php $this->load->view('layout/sidebar');?>
+         
+                <div class="main-content">
+                    <div class="container-fluid">
+                       
+                    <h1>Home</h1>    
+
+                <footer class="footer">
+                    <div class="w-100 clearfix">
+                        <span class="text-center text-sm-left d-md-inline-block">Copyright © <?php echo (date('Y'));?> ThemeKit v2.0. All Rights Reserved.</span>
+                        <span class="float-none float-sm-right mt-1 mt-sm-0 text-center">Customizado <i class="fa fa-car-side text-danger"></i> por <a href="http://lavalite.org/" class="text-dark" target="_blank">Edgleidson</a></span>
+                    </div>
+                </footer>
+                
+            </div>
+        </div>
+        
+        
+        
+
+        
---
+++ views/layout/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Estacionamento &nbsp;|&nbsp;<?php echo (isset($titulo) ? $titulo : 'Padrão'); ?></title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+
+        <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
+        
+        <link rel="stylesheet" href="<?php echo base_url('public/plugins/bootstrap/dist/css/bootstrap.min.css');?>">
+        <link rel="stylesheet" href="<?php echo base_url('public/plugins/fontawesome-free/css/all.min.css');?>">
+        <link rel="stylesheet" href="<?php echo base_url('public/plugins/icon-kit/dist/css/iconkit.min.css');?>">
+        <link rel="stylesheet" href="<?php echo base_url('public/plugins/ionicons/dist/css/ionicons.min.css');?>">
+        <link rel="stylesheet" href="<?php echo base_url('public/plugins/perfect-scrollbar/css/perfect-scrollbar.css');?>">         -->
+        <link rel="stylesheet" href="<?php echo base_url('public/dist/css/theme.min.css');?>">
+    </head>
+
+    <body>        
+        <div class="wrapper">
